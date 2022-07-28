bfee2899fe9ee6c90e3a155e0cdd659d001ba0e7
anandhm1 <101714009+anandhm1@users.noreply.github.com>
2022-07-28 13:54:35 +0530

    Update tests.py
    anand

--- restaurants/restaurantsmenu/menu/tests.py
+++ restaurants/restaurantsmenu/menu/tests.py
@@ -18,3 +18,16 @@ class ItemTestCase(TestCase):
     def test_item_info(self):
         qs = Item.objects.all()
         self.assertEqual(qs.count(),1)
+
+        
+        
+class ModifiersTestCase(TestCase):
+    def setUp(self):
+        self.section = Section.objects.create(name = "Dinner", description="topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high")
+        self.item = Item.objects.create(section=self.section,name="pizza",description="Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough",price= "200")
+        self.modifiers = Modifiers.objects.create(name="extra spicy", description="NongShim Shin Ramyum Super Spicy Noodle Soup Korean style instant noodles are easy to make and ready in a couple of minutes. These noodles have a great taste", price="20")
+
+    def test_item_info(self):
+        self.modifiers.item.set([self.item.pk])
+        qs = Modifiers.objects.all()
+        self.assertEqual(qs.count(),1)

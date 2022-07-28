fa80c94cddf78cf26ccb1ff91c6d595577e9ff4c
anandhm1 <101714009+anandhm1@users.noreply.github.com>
2022-07-28 09:50:06 +0530

    Update tests.py
    ANAND

--- restaurants/restaurantsmenu/menu/tests.py
+++ restaurants/restaurantsmenu/menu/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Item,Section,Modifiers
 
-# Create your tests here.
+
+class SectionTestCase(TestCase):
+    def setUp(self):
+        Section.objects.create(name = "Dinner", description="topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high")
+
+    def test_item_info(self):
+        qs = Section.objects.all()
+        self.assertEqual(qs.count(),1)
+
+class ItemTestCase(TestCase):
+    def setUp(self):
+        self.section=Section.objects.create(name = "Dinner", description="topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high")
+        Item.objects.create(section=self.section,name="pizza",description="Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough",price= "200")
+
+    def test_item_info(self):
+        qs = Item.objects.all()
+        self.assertEqual(qs.count(),1)
